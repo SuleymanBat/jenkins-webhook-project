279b1a07266b1b63ff4d93f108bea176581371c9
suleyman <snfeme5@gmail.com>
2022-10-11 05:30:03 +0200

    python file updated

--- kaldrmlar.py
+++ kaldrmlar.py
@@ -3,3 +3,4 @@ import sys
 print sys.getdefaultencoding()
 
 print('Ne sabahı göreyim, ne sabah görüneyim,Gündüzler size kalsın, verin karanlıkları')
+print('Islak bir yorgan gibi, sımsıkı bürüneyim,Örtün, üstüme örtün, serin karanlıkları')
